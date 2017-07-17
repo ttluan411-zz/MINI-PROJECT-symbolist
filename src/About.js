1a6e49a3d47d242ac0e15c9593dbc0aa6ad72216
Logan Truong <ttluan411@gmail.com>
2017-07-16 22:05:06 -0600

    Updates

--- src/About.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import './main.css';
-
-export default function(){
-    return(
-          <div className="About"><p>Just a simple stocks watch list</p></div>
-    )
-}
--- src/service.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function fetchStock(abc){
-     return axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=${abc}&apikey=U1TEZICKW8CON2ZU`).then(res => res.data)
-}
-
-export function getStock(abc){
-    return axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${abc}&outputsize=full&apikey=U1TEZICKW8CON2ZU`).then(res => res.data)
-}
